c456e2523ecc180af8f6cd0b4021b57272a06b60
MikhailApsalikov <Mikhail_Apsalikov@epam.com>
2016-04-09 16:54:02 +0300

    Added ISelpRepository main methods

--- Selp/Selp.Interfaces/ISelpRepository.cs
+++ Selp/Selp.Interfaces/ISelpRepository.cs
@@ -1,6 +1,25 @@
 ﻿namespace Selp.Interfaces
 {
+	using System;
+	using System.Linq;
+	using System.Linq.Expressions;
+	using Entities;
+
 	public interface ISelpRepository<TEntity, TKey> where TEntity : ISelpEntitiy<TKey>
 	{
+		IQueryable<TEntity> GetAll();
+		TEntity GetById(int id);
+
+		IQueryable<TEntity> GetByFilter(BaseFilter filter);
+
+		IQueryable<TEntity> GetByCustomExpression(Expression<Func<TEntity, bool>> filter);
+
+		TEntity Create(TEntity item);
+
+		TEntity Update(TEntity item);
+
+		void Remove(TEntity item);
+
+		void RemoveById(TKey key);
 	}
 }
--- Selp/Selp.Repository/SelpRepository.cs
+++ Selp/Selp.Repository/SelpRepository.cs
@@ -1,12 +1,51 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Selp.Repository
+﻿namespace Selp.Repository
 {
-	public class SelpRepository
+	using System;
+	using System.Linq;
+	using System.Linq.Expressions;
+	using Entities;
+	using Interfaces;
+
+	public class SelpRepository<TEntity, TKey> : ISelpRepository<TEntity, TKey> where TEntity : ISelpEntitiy<TKey>
 	{
+		public IQueryable<TEntity> GetAll()
+		{
+			throw new NotImplementedException();
+		}
+
+		public TEntity GetById(int id)
+		{
+			throw new NotImplementedException();
+		}
+
+		public IQueryable<TEntity> GetByCustomExpression(Expression<Func<TEntity, bool>> filter)
+		{
+			throw new NotImplementedException();
+		}
+
+		public TEntity Create(TEntity item)
+		{
+			throw new NotImplementedException();
+		}
+
+		public TEntity Update(TEntity item)
+		{
+			throw new NotImplementedException();
+		}
+
+		public void Remove(TEntity item)
+		{
+			throw new NotImplementedException();
+		}
+
+		public void RemoveById(TKey key)
+		{
+			throw new NotImplementedException();
+		}
+
+		public IQueryable<TEntity> GetByFilter(BaseFilter filter)
+		{
+			throw new NotImplementedException();
+		}
 	}
 }
