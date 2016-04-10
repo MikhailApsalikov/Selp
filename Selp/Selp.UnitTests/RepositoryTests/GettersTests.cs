aeb74f04adb4fc48b9996d4adaaf9a3f25e6e186
Mikhail Apsalikov <Mikhail_Apsalikov@epam.com>
2016-04-10 15:02:04 +0300

    Added infrastructure for Repository Getters tests

--- Selp/Selp.UnitTests/RepositoryTests/GettersTests.cs
+++ Selp/Selp.UnitTests/RepositoryTests/GettersTests.cs
@@ -1,12 +1,53 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Selp.UnitTests.RepositoryTests
+﻿namespace Selp.UnitTests.RepositoryTests
 {
-	class GettersTests
+	using System.Collections.Generic;
+	using System.Data.Entity;
+	using System.Linq;
+	using Fake;
+	using Microsoft.VisualStudio.TestTools.UnitTesting;
+	using Moq;
+	using Repository;
+
+	[TestClass]
+	public class GettersTests
 	{
+		private IDbSet<FakeEntity> dbSet;
+		private SelpRepository<FakeEntity, int> repository;
+
+
+		[TestInitialize]
+		public void Initialize()
+		{
+			var testData = new List<FakeEntity>();
+			for (var i = 0; i < 150; i++)
+			{
+				testData.Add(new FakeEntity
+				{
+					Id = i,
+					Name = "Entity " + i.ToString(),
+					Description = null,
+					IsDeleted = false
+				});
+			}
+			IQueryable<FakeEntity> fakeList = testData.AsQueryable();
+
+			var dbSetMock = new Mock<IDbSet<FakeEntity>>();
+			dbSetMock.Setup(m => m.Provider).Returns(fakeList.Provider);
+			dbSetMock.Setup(m => m.Expression).Returns(fakeList.Expression);
+			dbSetMock.Setup(m => m.ElementType).Returns(fakeList.ElementType);
+			dbSetMock.Setup(m => m.GetEnumerator()).Returns(fakeList.GetEnumerator());
+			dbSet = dbSetMock.Object;
+
+			var dbContextMock = new Mock<FakeDbContext>();
+			dbContextMock
+				.Setup(x => x.FakeEntities)
+				.Returns(dbSet);
+
+			var mock = new Mock<SelpRepository<FakeEntity, int>>();
+			mock.SetupGet(d => d.IsRemovingFake).Returns(false);
+			mock.SetupGet(d => d.DbContext).Returns(dbContextMock.Object);
+			mock.SetupGet(d => d.DbSet).Returns(dbSet);
+			repository = mock.Object;
+		}
 	}
 }
