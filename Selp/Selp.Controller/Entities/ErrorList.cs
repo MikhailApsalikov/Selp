deb92710fc8668a12d71fd1a11781ad4b189ef7f
Mikhail Apsalikov <Mikhail_Apsalikov@epam.com>
2016-04-12 17:30:09 +0300

    Implemented Repository

---
+++ Selp/Selp.Controller/Entities/ErrorList.cs
@@ -0,0 +1,18 @@
+﻿// ReSharper disable InconsistentNaming
+
+namespace Selp.Controller.Entities
+{
+	using System.Collections.Generic;
+	using Selp.Entities;
+
+	internal class ErrorList
+	{
+		public ErrorList(IEnumerable<ValidatorError> errors)
+		{
+			this.errors = errors;
+		}
+
+		public IEnumerable<ValidatorError> errors { get; set; }
+		public bool isValid => false;
+	}
+}
--- Selp/Selp.Controller/SelpController.cs
+++ Selp/Selp.Controller/SelpController.cs
@@ -1,50 +1,128 @@
 ﻿namespace Selp.Controller
 {
 	using System;
+	using System.Collections.Generic;
+	using System.Linq;
+	using System.Net;
 	using System.Web.Http;
+	using Common.Exceptions;
 	using Entities;
 	using Interfaces;
+	using Selp.Entities;
 
-	public abstract class SelpController<TModel, TEntity, TKey> : ISelpController<TModel, TEntity, TKey>
+	public abstract class SelpController<TModel, TEntity, TKey> : ApiController, ISelpController<TModel, TEntity, TKey>
 		where TModel : ISelpEntitiy<TKey> where TEntity : ISelpEntitiy<TKey>
 	{
+		protected SelpController(ISelpRepository<TEntity, TKey> repository)
+		{
+			Repository = repository;
+		}
+
+		protected ISelpRepository<TEntity, TKey> Repository { get; }
+
 		public abstract string ControllerName { get; }
-
-		[NonAction]
-		protected virtual TModel MapEntityToModel(TEntity entity)
-		{
-			throw new NotImplementedException();
-		}
-
-		[NonAction]
-		protected virtual TEntity MapModelToEntity(TModel entity)
-		{
-			throw new NotImplementedException();
-		}
 
 		public virtual IHttpActionResult Delete(TKey id)
 		{
-			throw new NotImplementedException();
+			try
+			{
+				Repository.Remove(id);
+				return Ok();
+			}
+			catch (Exception e)
+			{
+				return HandleException(e);
+			}
 		}
 
 		public virtual IHttpActionResult Get(TKey id)
 		{
-			throw new NotImplementedException();
+			try
+			{
+				return Ok(MapEntityToModel(Repository.GetById(id)));
+			}
+			catch (Exception e)
+			{
+				return HandleException(e);
+			}
 		}
 
 		public virtual IHttpActionResult Get([FromUri] BaseFilter query)
 		{
-			throw new NotImplementedException();
+			try
+			{
+				IQueryable<TEntity> entities = Repository.GetByFilter(query);
+				var models = new List<TModel>();
+				foreach (TEntity entity in entities)
+				{
+					models.Add(MapEntityToModel(entity));
+				}
+
+				return Ok(models);
+			}
+			catch (Exception e)
+			{
+				return HandleException(e);
+			}
 		}
 
 		public virtual IHttpActionResult Post([FromBody] TModel value)
 		{
-			throw new NotImplementedException();
+			try
+			{
+				RepositoryModifyResult<TEntity> result = Repository.Create(MapModelToEntity(value));
+				if (result.ModifiedEntity != null)
+				{
+					return Created(new Uri($"{ControllerName}/{result.ModifiedEntity.Id}", UriKind.Relative),
+						MapEntityToModel(result.ModifiedEntity));
+				}
+
+				return Content(HttpStatusCode.InternalServerError, new ErrorList(result.Errors));
+			}
+			catch (Exception e)
+			{
+				return HandleException(e);
+			}
 		}
 
 		public virtual IHttpActionResult Put(TKey id, [FromBody] TModel value)
 		{
-			throw new NotImplementedException();
+			try
+			{
+				value.Id = id;
+				RepositoryModifyResult<TEntity> result = Repository.Update(id, MapModelToEntity(value));
+				if (result.ModifiedEntity != null)
+				{
+					return Ok(MapEntityToModel(result.ModifiedEntity));
+				}
+
+				return Content(HttpStatusCode.InternalServerError, new ErrorList(result.Errors));
+			}
+			catch (Exception e)
+			{
+				return HandleException(e);
+			}
 		}
+
+		protected virtual IHttpActionResult HandleException(Exception e)
+		{
+			if (e is NotSupportedException)
+			{
+				return StatusCode(HttpStatusCode.MethodNotAllowed);
+			}
+
+			if (e is EntityNotFoundException)
+			{
+				return NotFound();
+			}
+
+			return InternalServerError(e);
+		}
+
+		[NonAction]
+		protected abstract TModel MapEntityToModel(TEntity entity);
+
+		[NonAction]
+		protected abstract TEntity MapModelToEntity(TModel entity);
 	}
 }
