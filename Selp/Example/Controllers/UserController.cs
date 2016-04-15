ae3f9d046ceecb68df1b5971c3163a3423df058d
Mikhail Apsalikov <Mikhail_Apsalikov@epam.com>
2016-04-15 16:56:54 +0300

    Example application created. Created first controller there

---
+++ Selp/Example/Controllers/UserController.cs
@@ -0,0 +1,16 @@
+﻿namespace Example.Controllers
+{
+	using Models;
+	using Repository;
+	using Selp.Controller;
+	using Selp.Interfaces;
+
+	public class UserController : SelpController<UserModel, User, int>
+	{
+		public UserController(ISelpRepository<UserModel, User, int> repository) : base(repository)
+		{
+		}
+
+		public override string ControllerName => "User";
+	}
+}
