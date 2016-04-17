3ad21ad6fb80b3f86a74eeb5dc66804a906488aa
Mikhail Apsalikov <dee_jay@bk.ru>
2016-04-17 16:13:43 +0300

    Example application: DbContext created

---
+++ Selp/Example.Repositories/ExampleDbContext.cs
@@ -0,0 +1,17 @@
+﻿using System.Data.Entity;
+using Example.Entities;
+
+namespace Example.Repositories
+{
+	public class ExampleDbContext : DbContext
+	{
+		public IDbSet<User> Users { get; set; }
+		public IDbSet<Policy> Policies { get; set; }
+
+		public IDbSet<Party> Parties { get; set; }
+
+		public IDbSet<Document> Documents { get; set; }
+		public IDbSet<Region> Regions { get; set; }
+		public IDbSet<Attachment> Attachments { get; set; }
+	}
+}
