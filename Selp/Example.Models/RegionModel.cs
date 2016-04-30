7655a52f2e69629768b1f7ca699454e7b8a06c69
Mikhail Apsalikov <dee_jay@bk.ru>
2016-04-30 21:08:49 +0300

    Polygon project removed

--- Selp/Example.Models/RegionModel.cs
+++ Selp/Example.Models/RegionModel.cs
@@ -1,12 +1,9 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Example.Models
+﻿namespace Example.Models
 {
-	class RegionModel
+	public class RegionModel
 	{
+		public string Name { get; set; }
+
+		public int Id { get; set; }
 	}
 }
