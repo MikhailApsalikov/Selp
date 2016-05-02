9801676c63c9dc953d7b5ed1fd9f1351f662c241
Mikhail Apsalikov <dee_jay@bk.ru>
2016-05-02 15:26:28 +0300

    Example application: added progress bar to buttons on CTA page added mainController

---
+++ Selp/Example.Web/AngularApp/Controllers/mainController.js
@@ -0,0 +1,18 @@
+﻿(function () {
+    'use strict';
+
+    angular
+      .module('APP')
+      .controller('mainController', ['$scope', 'loginService', mainController]);
+
+    function mainController($scope, loginService) {
+        var vm = this;
+        $scope.isLoggedIn = loginService.isAuthenticated;
+
+        vm.title = '';
+
+        activate();
+
+        function activate() { }
+    }
+})();
