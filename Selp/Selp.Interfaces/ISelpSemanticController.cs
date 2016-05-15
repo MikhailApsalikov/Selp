4a0dfc9041019866d0565138a9c8fcc4f015feef
Mikhail Apsalikov <dee_jay@bk.ru>
2016-05-15 23:41:12 +0300

    Created ISelpSemanticController interface

---
+++ Selp/Selp.Interfaces/ISelpSemanticController.cs
@@ -0,0 +1,13 @@
+﻿using System.Web.Http;
+
+namespace Selp.Interfaces
+{
+    internal interface ISelpSemanticController<in TModel, in TKey> where TModel : ISelpEntity<TKey>
+    {
+        IHttpActionResult Get();
+
+        IHttpActionResult GetPredicate();
+
+        IHttpActionResult GetSubject();
+    }
+}
