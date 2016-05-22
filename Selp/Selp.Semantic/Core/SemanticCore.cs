b07bee6bcde33ac30cf02552764ccced4dd1f5ea
Mikhail Apsalikov <dee_jay@bk.ru>
2016-05-22 15:31:21 +0300

    Added fake implementation of SemanticCore Added empty SelpSemanticController

---
+++ Selp/Selp.Semantic/Core/SemanticCore.cs
@@ -0,0 +1,40 @@
+﻿namespace Selp.Semantic.Core
+{
+    using System;
+    using System.Reflection;
+    using VDS.RDF;
+
+    public static class SemanticCore<T>
+    {
+        public static IGraph GenerateGraph(T model)
+        {
+            var graph = new Graph();
+            Type modelType = model.GetType();
+            var subjectNode = graph.CreateUriNode(ConstructSubjectUrl(model));
+            foreach (PropertyInfo propertyInfo in modelType.GetProperties(BindingFlags.Instance | BindingFlags.Public))
+            {
+                var predicateNode = graph.CreateUriNode(ConstructPredicateUrl(propertyInfo));
+                var objectNode = ConstructObjectValue(propertyInfo.GetValue(model), graph);
+
+                graph.Assert(new Triple(subjectNode, predicateNode, objectNode));
+            }
+
+            return graph;
+        }
+
+        public static Uri ConstructSubjectUrl(T model)
+        {
+            return UriFactory.Create("http://localhost/subjectUrl");
+        }
+
+        public static Uri ConstructPredicateUrl(PropertyInfo property)
+        {
+            return UriFactory.Create("http://localhost/predicate/" + property.Name);
+        }
+
+        public static INode ConstructObjectValue(object value, Graph graph)
+        {
+            return graph.GetUriNode(UriFactory.Create("http://localhost/objectUrl"+value.ToString()));
+        }
+    }
+}
---
+++ Selp/Selp.Semantic/SelpSemanticController.cs
@@ -0,0 +1,41 @@
+﻿namespace Selp.Semantic
+{
+	using System;
+	using System.Web.Http;
+	using Interfaces;
+
+	public abstract class SelpSemanticController<TShortModel, TModel, TEntity, TKey> : ISelpSemanticController<TKey>
+		where TShortModel : ISelpEntity<TKey> where TModel : ISelpEntity<TKey> where TEntity : ISelpEntity<TKey>
+	{
+		protected SelpSemanticController(ISelpRepository<TEntity, TKey> repository)
+		{
+			Repository = repository;
+		}
+
+		protected ISelpRepository<TEntity, TKey> Repository { get; }
+
+		public IHttpActionResult Get(TKey id)
+		{
+			throw new NotImplementedException();
+		}
+
+		public IHttpActionResult Get()
+		{
+			throw new NotImplementedException();
+		}
+
+		public IHttpActionResult GetPredicate()
+		{
+			throw new NotImplementedException();
+		}
+
+		public IHttpActionResult GetSubject()
+		{
+			throw new NotImplementedException();
+		}
+
+		protected abstract TModel MapEntityToModel(TEntity entity);
+
+		protected abstract TShortModel MapEntityToShortModel(TEntity entity);
+	}
+}
