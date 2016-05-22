9fea2b3214e2dcd6b15f08f4a7e7ad54b03e0d63
Mikhail Apsalikov <dee_jay@bk.ru>
2016-05-23 00:43:16 +0300

    Added sanity tests for semantic controller

---
+++ Selp/Selp.UnitTests/Fake/FakeSemanticController.cs
@@ -0,0 +1,24 @@
+﻿namespace Selp.UnitTests.Fake
+{
+	using Interfaces;
+	using Semantic;
+	using VDS.RDF;
+
+	public class FakeSemanticController : SelpSemanticController<FakeEntity, FakeEntity, FakeEntity, int>
+	{
+		public FakeSemanticController(ISelpRepository<FakeEntity, int> repository, IRdfWriter rdfWriter)
+			: base(repository, rdfWriter)
+		{
+		}
+
+		protected override FakeEntity MapEntityToModel(FakeEntity entity)
+		{
+			return entity;
+		}
+
+		protected override FakeEntity MapEntityToShortModel(FakeEntity entity)
+		{
+			return entity;
+		}
+	}
+}
---
+++ Selp/Selp.UnitTests/SemanticTests/SanityTests.cs
@@ -0,0 +1,52 @@
+﻿namespace Selp.UnitTests.SemanticTests
+{
+	using System.Collections.Generic;
+	using Common.Entities;
+	using Fake;
+	using Interfaces;
+	using Microsoft.VisualStudio.TestTools.UnitTesting;
+	using Moq;
+	using Semantic.Core;
+	using VDS.RDF.Writing;
+
+	[TestClass]
+	public class SanityTests
+	{
+		internal FakeSemanticController Controller { get; set; }
+
+		static SanityTests()
+		{
+			SemanticTypeResolver.Instance.Register<FakeEntity>("http://www.fakeentity.com");
+		}
+
+		[TestInitialize]
+		public void Initialise()
+		{
+			var mock = new Mock<ISelpRepository < FakeEntity, int>> ();
+			mock.Setup((s) => s.GetAll()).Returns(() => new List<FakeEntity>()
+			{
+				new FakeEntity(),
+				new FakeEntity(),
+			});
+
+			mock.Setup((s) => s.GetById(It.IsAny<int>())).Returns<int>((i) => new FakeEntity()
+			{
+				Id = i
+			});
+
+			Controller = new FakeSemanticController(mock.Object, new RdfXmlWriter());
+		}
+
+		[TestMethod]
+		public void GetShouldWork()
+		{
+			Controller.Get();
+		}
+
+		[TestMethod]
+		public void GetByIdShouldWork()
+		{
+			Controller.Get(1);
+		}
+	}
+}
