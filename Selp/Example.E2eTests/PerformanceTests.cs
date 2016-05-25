9469a5be0d85032e3b03da02400eb12921cc2373
Mikhail Apsalikov <Mikhail_Apsalikov@epam.com>
2016-05-25 17:19:29 +0300

    Example application: added e2e performance sanity tests

---
+++ Selp/Example.E2eTests/PerformanceTests.cs
@@ -0,0 +1,81 @@
+﻿namespace Example.E2eTests
+{
+	using System;
+	using System.Diagnostics;
+	using System.Linq;
+	using System.Net;
+	using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+	[TestClass]
+	public class PerformanceTests
+	{
+		private const string ServerLocation = "http://localhost:33332/";
+		private const int RepeatTimes = 50;
+
+		/*
+		[TestMethod]
+		public void Web2GetAllPerformanceTest()
+		{
+			var client = new WebClient();
+			var result = Wrap(() => client.DownloadString($"{ServerLocation}/api/Region/"), RepeatTimes);
+			Console.WriteLine($"Web 2.0 average test result is {result} ms");
+		}
+
+		[TestMethod]
+		public void Web3GetAllPerformanceTest()
+		{
+			var client = new WebClient();
+			var result = Wrap(() => client.DownloadString($"{ServerLocation}/semantic/Region/"), RepeatTimes);
+			Console.WriteLine($"Web 3.0 average test result is {result} ms");
+		}
+
+		[TestMethod]
+		public void Web2GetByIdPerformanceTest()
+		{
+			var client = new WebClient();
+			var result = Wrap(() => client.DownloadString($"{ServerLocation}/api/User/admin"), RepeatTimes);
+			Console.WriteLine($"Web 2.0 average test result is {result} ms");
+		}
+
+		[TestMethod]
+		public void Web3GetByIdPerformanceTest()
+		{
+			var client = new WebClient();
+			var result = Wrap(() => client.DownloadString($"{ServerLocation}/semantic/User/admin"), RepeatTimes);
+			Console.WriteLine($"Web 3.0 average test result is {result} ms");
+		}*/
+
+		[TestMethod]
+		public void FullPerformanceTest()
+		{
+			var client = new WebClient();
+
+			client.DownloadString($"{ServerLocation}/api/Region/"); //тестовый прогон (чтобы избежать задержки 1 запуска)
+			client.DownloadString($"{ServerLocation}/semantic/Region/");
+
+			var result = Wrap(() => client.DownloadString($"{ServerLocation}/api/Region/"), RepeatTimes);
+			Console.WriteLine($"Web 2.0 GetAll выполняется в среднем за {result} миллисекунд");
+			result = Wrap(() => client.DownloadString($"{ServerLocation}/api/User/admin"), RepeatTimes);
+			Console.WriteLine($"Web 2.0 GetById выполняется в среднем за {result} миллисекунд");
+			result = Wrap(() => client.DownloadString($"{ServerLocation}/semantic/Region/"), RepeatTimes);
+			Console.WriteLine($"Web 3.0 GetAll выполняется в среднем за {result} миллисекунд");
+			result = Wrap(() => client.DownloadString($"{ServerLocation}/semantic/User/admin"), RepeatTimes);
+			Console.WriteLine($"Web 3.0 GetById выполняется в среднем за {result} миллисекунд");
+		}
+
+		private double Wrap(Action action, int repeatTimes)
+		{
+			var results = new long[repeatTimes];
+
+			for (var i = 0; i < repeatTimes; i++)
+			{
+				Stopwatch sw = Stopwatch.StartNew();
+				action();
+				sw.Stop();
+				results[i] = sw.ElapsedMilliseconds;
+			}
+
+			return results.Sum()/(double) repeatTimes;
+		}
+	}
+}
--- Selp/Selp.Semantic/SelpSemanticController.cs
+++ Selp/Selp.Semantic/SelpSemanticController.cs
@@ -1,67 +1,66 @@
 ﻿namespace Selp.Semantic
 {
-    using System;
-    using System.Collections.Generic;
-    using System.Linq;
-    using System.Net.Http;
-    using System.Text;
-    using System.Web.Http;
-    using Core;
-    using Interfaces;
-    using VDS.RDF;
-    using VDS.RDF.Writing;
+	using System.Collections.Generic;
+	using System.Linq;
+	using System.Net.Http;
+	using System.Text;
+	using System.Web.Http;
+	using Core;
+	using Interfaces;
+	using VDS.RDF;
+	using VDS.RDF.Writing;
 
-    public abstract class SelpSemanticController<TShortModel, TModel, TEntity, TKey> : ApiController,
-        ISelpSemanticController<TKey>
-        where TShortModel : ISelpEntity<TKey> where TModel : ISelpEntity<TKey> where TEntity : ISelpEntity<TKey>
-    {
-        protected SelpSemanticController(ISelpRepository<TEntity, TKey> repository, IRdfWriter rdfWriter)
-        {
-            Repository = repository;
-            RdfWriter = rdfWriter;
-        }
+	public abstract class SelpSemanticController<TShortModel, TModel, TEntity, TKey> : ApiController,
+		ISelpSemanticController<TKey>
+		where TShortModel : ISelpEntity<TKey> where TModel : ISelpEntity<TKey> where TEntity : ISelpEntity<TKey>
+	{
+		protected SelpSemanticController(ISelpRepository<TEntity, TKey> repository, IRdfWriter rdfWriter)
+		{
+			Repository = repository;
+			RdfWriter = rdfWriter;
+		}
 
-        protected ISelpRepository<TEntity, TKey> Repository { get; }
+		protected ISelpRepository<TEntity, TKey> Repository { get; }
 
-        protected IRdfWriter RdfWriter { get; }
+		protected IRdfWriter RdfWriter { get; }
 
-        public HttpResponseMessage Get(TKey id)
-        {
-            TEntity entity = Repository.GetById(id);
-            TModel model = MapEntityToModel(entity);
-            IGraph graph = SemanticCore<TModel, TKey>.GenerateGraph(model);
-            return new HttpResponseMessage
-            {
-                Content = new StringContent(
-                    StringWriter.Write(graph, RdfWriter),
-                    Encoding.Unicode,
-                    GetMimeType()
-                    )
-            };
-        }
+		public HttpResponseMessage Get(TKey id)
+		{
+			TEntity entity = Repository.GetById(id);
+			TModel model = MapEntityToModel(entity);
+			IGraph graph = SemanticCore<TModel, TKey>.GenerateGraph(model);
+			return new HttpResponseMessage
+			{
+				Content = new StringContent(
+					StringWriter.Write(graph, RdfWriter),
+					Encoding.Unicode,
+					GetMimeType()
+					)
+			};
+		}
 
-        public HttpResponseMessage Get()
-        {
-            List<TEntity> entities = Repository.GetAll();
-            List<TShortModel> model = entities.Select(MapEntityToShortModel).ToList();
-            IGraph graph = SemanticCore<TShortModel, TKey>.GenerateGraph(model);
-            return new HttpResponseMessage
-            {
-                Content = new StringContent(
-                    StringWriter.Write(graph, RdfWriter),
-                    Encoding.Unicode,
-                    GetMimeType()
-                    )
-            };
-        }
+		public HttpResponseMessage Get()
+		{
+			List<TEntity> entities = Repository.GetAll();
+			List<TShortModel> model = entities.Select(MapEntityToShortModel).ToList();
+			IGraph graph = SemanticCore<TShortModel, TKey>.GenerateGraph(model);
+			return new HttpResponseMessage
+			{
+				Content = new StringContent(
+					StringWriter.Write(graph, RdfWriter),
+					Encoding.Unicode,
+					GetMimeType()
+					)
+			};
+		}
 
-        protected virtual string GetMimeType()
-        {
-            return "application/rdf+xml";
-        }
+		protected virtual string GetMimeType()
+		{
+			return "application/rdf+xml";
+		}
 
-        protected abstract TModel MapEntityToModel(TEntity entity);
+		protected abstract TModel MapEntityToModel(TEntity entity);
 
-        protected abstract TShortModel MapEntityToShortModel(TEntity entity);
-    }
+		protected abstract TShortModel MapEntityToShortModel(TEntity entity);
+	}
 }
